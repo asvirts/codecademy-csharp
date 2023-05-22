b217c723643acd4dda6776bc9a6b2dabf3ce7372
Andrew Virts <asvirts@outlook.com>
2023-05-21 20:29:53 -0500

    Money Maker

--- money-maker.cs
+++ money-maker.cs
@@ -2,12 +2,22 @@ using System;
 
 namespace MoneyMaker
 {
-    class MainClass
+  class MainClass
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Money Maker!");
+      Console.WriteLine("Welcome to Money Maker!\nPlease enter a number to convert to coins: ");
+      double UserNum = Convert.ToDouble(Console.ReadLine());
 
-        }
+      double GoldCoins = Math.Floor(UserNum / 10);
+      double remainder = GoldCoins % UserNum;
+
+      double SilverCoins = Math.Floor(remainder % 5);
+      double SilverRemainder = SilverCoins % 1;
+
+      double BronzeCoins = Math.Floor(remainder % 5);
+
+      Console.WriteLine($"{UserNum} cents is equal to {GoldCoins} gold coins, {SilverCoins} silver coins, and {BronzeCoins} bronze coins");
     }
+  }
 }
